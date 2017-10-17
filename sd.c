937b9bd2639d336094277d3965d5290da494aae5
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2017-10-17 01:39:00 -0700

    Create sd.c

---
+++ sd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,s=0,sum1=0;
+for(i=1;i<=15;i++)
+{
+s=s+i;
+}
+printf(%d",s);
+for(k=15;k<=45;k++)
+{
+if(k%2!=0)
+{
+sum1=sum1+k;
+}
+}
+printf("%d",sum1);
+getch();
+}
