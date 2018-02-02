dec8d3e711a72fd0ae27e87fe91dbcab060dd97e
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-02-02 15:06:33 +0530

    Create s7.c

---
+++ s7.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    int b,n=0,i,c,m=0;
+    char a[100]="sampath12/*-3";
+    char j;
+    b=strlen(a);
+    for(i=0;i<=b;i++)
+    {
+        if(isalpha(a[i]))
+        
+        {
+            n++;
+        }
+       else
+        {
+            m++;
+        }
+        
+    }
+    
+    printf(" ttl space=%d",m-1);
+
+}
