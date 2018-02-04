be00870ff80926d99828a07d2a5d26c1b4710f56
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-02-04 20:28:13 +0530

    Create s9.c

---
+++ s9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    char a[100]="sampath";
+    printf("how many time ")
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%s\n",a);
+    }
+}
