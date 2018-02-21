aca6e8e448f485b1e229c7e186f6e1db6be896e3
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-02-21 16:09:55 +0530

    Create f.c

---
+++ f.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,temp,j,n=10;
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(a[i]>=a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+//for(i=0;i<=10;i++)
+    
+      printf("%d",a[n]);
+       
+}
