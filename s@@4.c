2895919dbc2c97413df066fd280a36363501693d
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-03-02 16:15:53 +0530

    Create s@@4.c

---
+++ s@@4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,i1,s=0;
+    scanf("%d %d",&i,&i1);
+    s=i+i1;
+    if(s%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("No");
+    }
+}
+
+    
