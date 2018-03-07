964883d1491431e8a7ed74f37362f0aa9a9b19b6
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-03-07 16:13:10 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,n;
+    scanf("%d",&n);
+    scanf("%d%d",&a,&b);
+ if((a<n)&&(b>n))
+ {
+     printf("yes");
+ }
+ else
+ {
+     printf("no");
+ }
+}
