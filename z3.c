04324e8b0fe5dacf26742f31db720c4faec3ad49
sampathfzs <31653428+sampathfzs@users.noreply.github.com>
2018-03-14 16:26:40 +0530

    Create z3.c

---
+++ z3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,a1[100],j,i,k,b,g,g1;
+scanf("%d%d",&a,&b);
+for(i=2;i<=a;i++)
+{
+    g=a%i;
+    g1=a%i;
+    if((g==0)&&(g1==0))
+    {
+        a1[j]=i;
+        k=j;
+        j++;
+    }
+}
+printf("%d",a1[k]);
+}
